03177134b83e2a0de18a5f0f3721e9ed3f2de708
Tomás Altamirano <tomas.altamirano@anahuac.mx>
2026-01-27 12:51:37 -0500

    Implement Adapter pattern in Java

---
+++ patterns/structural/adapter/code/java/LegacyNotificationService.java
@@ -0,0 +1,5 @@
+public class LegacyNotificationService {
+    public void sendOldSchool(String msg, String recipient) {
+        System.out.println("LEGACY SERVICE >>> Sending to " + recipient + ": " + msg);
+    }
+}
