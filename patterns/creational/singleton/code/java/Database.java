59d77e6a181d8cdf998008cd6fac70b63ec1cae2
isabellasaiden <isamedinasaiden@hotmail.com>
2026-01-27 18:20:56 -0600

    Add Java code for Singleton

---
+++ patterns/creational/singleton/code/java/Database.java
@@ -0,0 +1,15 @@
+public class Database {
+    private static Database instance;
+
+    private Database() { }
+
+    public static Database getInstance() {
+        if (instance == null) instance = new Database();
+        return instance;
+    }
+
+    public void query(String sql) {
+        System.out.println("[DB] Executing: " + sql);
+    }
+}
+
